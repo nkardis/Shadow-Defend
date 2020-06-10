7e90cc590948a13d304173095a1c23de00f6124a
Nicholas Kardis <nkardis@student.unimelb.edu.au>
2020-06-10 21:47:45 +1000

    Different slicers working

--- src/lists/Slicer.java
+++ src/lists/Slicer.java
@@ -11,7 +11,10 @@ import java.util.List;
 public class Slicer extends Sprite {
 
     private static final String IMAGE_FILE = "res/images/slicer.png";
-    private static final double SPEED = 1;
+    private static final double SPEED = 2;
+    private static final double REWARD = 2;
+    private static final double HEALTH = 1;
+    private static final int LIFE_LOSS = 1;
 
     private final List<Point> polyline;
     private int targetPointIndex;
--- src/lists/SuperSlicer.java
+++ src/lists/SuperSlicer.java
@@ -1,21 +1,67 @@
 package lists;
 
 import bagel.Input;
+import bagel.util.Point;
+import bagel.util.Vector2;
 
-/*
-public class SuperSlicer extends Slicer {
+import java.util.List;
+
+
+public class SuperSlicer extends Sprite {
     private static final String IMAGE_FILE = "res/images/superslicer.png";
     private static final double SPEED = 1.5;
     private static final double REWARD = 15;
     private static final double HEALTH = 1;
     private static final int LIFE_LOSS = 2;
 
-    public SuperSlicer() {
+    private final List<Point> polyline;
+    private int targetPointIndex;
+    private boolean finished;
+    private boolean hit;
 
+
+    public SuperSlicer(List<Point> polyline) {
+        super(polyline.get(0), IMAGE_FILE);
+        this.polyline = polyline;
+        this.targetPointIndex = 1;
+        this.finished = false;
     }
 
     @Override
     public void update(Input input) {
+        if (finished) {
+            return;
+        }
+        // Obtain where we currently are, and where we want to be
+        Point currentPoint = getCenter();
+        Point targetPoint = polyline.get(targetPointIndex);
+        // Convert them to vectors to perform some very basic vector math
+        Vector2 target = targetPoint.asVector();
+        Vector2 current = currentPoint.asVector();
+        Vector2 distance = target.sub(current);
+        // Distance we are (in pixels) away from our target point
+        double magnitude = distance.length();
+        // Check if we are close to the target point
+        if (magnitude < SPEED * ShadowDefend.getTimescale()) {
+            // Check if we have reached the end
+            if (targetPointIndex == polyline.size() - 1) {
+                finished = true;
+                return;
+            } else {
+                // Make our focus the next point in the polyline
+                targetPointIndex += 1;
+            }
+        }
+        // Move towards the target point
+        // We do this by getting a unit vector in the direction of our target, and multiplying it
+        // by the speed of the slicer (accounting for the timescale)
+        super.move(distance.normalised().mul(SPEED * ShadowDefend.getTimescale()));
+        // Update current rotation angle to face target point
+        setAngle(Math.atan2(targetPoint.y - currentPoint.y, targetPoint.x - currentPoint.x));
+        super.update(input);
 
     }
-}*/
+    public boolean isFinished() {
+        return finished;
+    }
+}
